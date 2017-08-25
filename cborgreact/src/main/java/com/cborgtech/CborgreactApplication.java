d409926c733306bd900a1e487ce7a2afdc02b600
seufster <seufster@gmail.com>
2017-08-24 21:05:39 -0400

    test

--- cborgreact/src/main/java/com/cborgtech/CborgreactApplication.java
+++ cborgreact/src/main/java/com/cborgtech/CborgreactApplication.java
@@ -2,12 +2,19 @@ package com.cborgtech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.cborgtech","org.toasthub"})
-public class CborgreactApplication {
+public class CborgreactApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CborgreactApplication.class);
+	} 
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(CborgreactApplication.class, args);
 	}
--- cborgreact/src/main/js/index.js
+++ cborgreact/src/main/js/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDom from 'react-dom';
 import AwesomeComponent from './AwesomeComponent.js';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <p>
-          Hello React test tests</p>
-        <AwesomeComponent/>
-      </div>
+    	<nav id="mainNav" className="navbar navbar-Public navbar-custom navbar-fixed-top affix-top">
+    		<div className="container">
+    			<div className="navbar-header page-scroll">
+    				<button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+    					<span className="sr-only">Toggle navigation</span> Menu <i className="fa fa-bars"></i>
+    				</button>
+    				<a className="navbar-brand page-scroll" href="#page-top">CBORGTECHxx</a>
+    			</div>
+    			<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    				<ul className="nav navbar-nav navbar-right">
+    					<li><a className="page-scroll" href="#services">Services</a></li>
+    					<li><a className="page-scroll" href="#portfolio">Portfolio</a></li>
+    					<li><a className="page-scroll" href="#about">About</a></li>
+    					<li><a className="page-scroll" href="#team">Team</a></li>
+    					<li><a className="page-scroll" href="/login/login.html">Login</a></li>
+    				</ul>
+    			</div>
+    		</div>
+    	</nav>
     );
   }
 }
 
-render(
-  <App/>, document.getElementById('app'));
+ReactDom.render( <App/>, document.getElementById('app') );
