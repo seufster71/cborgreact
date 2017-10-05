ba4df16f85d0f104096598f34fbaa5514cb59645
seufster <seufster@gmail.com>
2017-10-05 19:43:07 -0400

    update build process

--- cborgreact/src/main/js/index.js
+++ cborgreact/src/main/js/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import AwesomeComponent from './AwesomeComponent.js';
+import Theme from './theme.css';
 
 class App extends React.Component {
   render() {
--- cborgreact/webpack.config.js
+++ cborgreact/webpack.config.js
@@ -5,28 +5,41 @@ var BUILD_DIR = path.resolve(__dirname, 'src/main/resources/static/build');
 var APP_DIR = path.resolve(__dirname, 'src/main/js');
 
 var config = {
-	entry: APP_DIR + '/index.js',
-	devtool: 'sourcemaps',
+	entry: { app: APP_DIR + '/index.js' },
+	devtool: 'inline-source-map',
+	devServer: {
+		inline: true,
+		contentBase: './src/main/resources/static',
+	    proxy: { "/api/**": { target: 'http://localhost:8090', secure: false }  }
+	},
 	output: {
 	    path: BUILD_DIR,
-	    filename: 'bundle.js'
+	    filename: 'bundle.js',
+			publicPath: 'build/'
 	},
 	module : {
 		loaders : [
 			{
-				test : /\.js?/,
+				test : /\.jsx?/,
 				include: APP_DIR,
 				loader: 'eslint-loader'
 			},
-	      {
-	        test : /\.js?/,
-	        include : APP_DIR,
-	        loader : 'babel-loader',
-	        query: {
-	                    presets: ['es2015', 'react']
-	                }
-	      }
-	    ]
+	    {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader',
+        query: { presets: ['es2015', 'react'] }
+	    },
+			{
+				test: /\.css$/,
+				loader: "style-loader!css-loader"
+			},
+			{
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: { limit: 10000 }
+		   }
+	  ]
 	}
 };
 
