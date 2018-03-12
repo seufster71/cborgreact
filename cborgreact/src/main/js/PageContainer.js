cc329855f16e2a21f9c8e5ceb26bb953cc80f8ea
cborgtech <guru@cborgtech.com>
2018-03-11 21:54:23 -0400

    familiarizing w/ code.  testing small change

--- cborgreact/src/main/js/PageContainer.js
+++ cborgreact/src/main/js/PageContainer.js
@@ -1,52 +1,81 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import NavigationContainer from './core/navigation/NavigationContainer';
-import LoginContainer from './core/usermanagement/LoginContainer';
-import StatusView from './coreWeb/status/StatusView';
-import MemberContainer from './member/MemberContainer';
-import {bindActionCreators} from 'redux';
-import * as navActions from './core/navigation/navActions';
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import NavigationContainer from "./core/navigation/NavigationContainer";
+import LoginContainer from "./core/usermanagement/LoginContainer";
+import StatusView from "./coreWeb/status/StatusView";
+import MemberContainer from "./member/MemberContainer";
+import { bindActionCreators } from "redux";
+import * as navActions from "./core/navigation/navActions";
 
 class PageContainer extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
     this.navigationChange = this.navigationChange.bind(this);
-	}
+  }
 
   navigationChange(event) {
-    if (event.target.id == 'LOGIN') {
-      this.props.actions.navChange({currentPage:'login'});
+    if (event.target.id == "LOGIN") {
+      this.props.actions.navChange({ currentPage: "login" });
     }
   }
 
   render() {
-    if (this.props.navigation.currentPage == 'login') {
-      return (<div><NavigationContainer navClick={this.navigationChange} menuName="PUBLIC_MENU_RIGHT"/><StatusView/><LoginContainer/></div>);
-    } else if (this.props.navigation.currentPage == 'member') {
-      return (<div><NavigationContainer navClick={this.navigationChange} menuName="MEMBER_MENU_RIGHT"/><StatusView/><MemberContainer/></div>);
+    if (this.props.navigation.currentPage == "login") {
+      return (
+        <div>
+          <NavigationContainer
+            navClick={this.navigationChange}
+            menuName="PUBLIC_MENU_RIGHT"
+          />
+          <StatusView />
+          <LoginContainer />
+        </div>
+      );
+    } else if (this.props.navigation.currentPage == "member") {
+      return (
+        <div>
+          <NavigationContainer
+            navClick={this.navigationChange}
+            menuName="MEMBER_MENU_RIGHT"
+          />
+          <StatusView />
+          <MemberContainer />
+        </div>
+      );
     } else {
-      return (<div><NavigationContainer navClick={this.navigationChange} menuName="PUBLIC_MENU_RIGHT"/><StatusView/>Main Page</div>);
+      return (
+        <div>
+          <NavigationContainer
+            navClick={this.navigationChange}
+            menuName="PUBLIC_MENU_RIGHT"
+          />
+          <StatusView />Test Change
+        </div>
+      );
     }
-
   }
 }
 
 PageContainer.propTypes = {
-	appPrefs: PropTypes.object.isRequired,
-	navigation: PropTypes.object.isRequired,
-	menus: PropTypes.object,
-	lang: PropTypes.string,
-	actions: PropTypes.object
+  appPrefs: PropTypes.object.isRequired,
+  navigation: PropTypes.object.isRequired,
+  menus: PropTypes.object,
+  lang: PropTypes.string,
+  actions: PropTypes.object
 };
 
 function mapStateToProps(state, ownProps) {
-  return {menus:state.appMenus.menus, lang:state.lang, appPrefs:state.appPrefs, navigation:state.navigation};
+  return {
+    menus: state.appMenus.menus,
+    lang: state.lang,
+    appPrefs: state.appPrefs,
+    navigation: state.navigation
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-  return { actions:bindActionCreators(navActions,dispatch) };
+  return { actions: bindActionCreators(navActions, dispatch) };
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(PageContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(PageContainer);
--- cborgreact/src/main/js/app.js
+++ cborgreact/src/main/js/app.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
-import { render } from 'react-dom';
-import configureStore from './store/configureStore';
-import {Provider} from 'react-redux';
-import {initPublic} from './core/common/appPrefActions';
-import {sessionCheck} from './member/session/sessionActions';
-import PageContainer from './PageContainer.js';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
-import Theme from './theme.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import configureStore from "./store/configureStore";
+import { Provider } from "react-redux";
+import { initPublic } from "./core/common/appPrefActions";
+import { sessionCheck } from "./member/session/sessionActions";
+import PageContainer from "./PageContainer.js";
+import Bootstrap from "bootstrap/dist/css/bootstrap.css";
+import Theme from "./theme.css";
 
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
+if (process.env.NODE_ENV !== "production") {
+  console.log("Looks like we are in development mode!");
 }
 
 const store = configureStore();
@@ -17,22 +17,22 @@ store.dispatch(initPublic());
 store.dispatch(sessionCheck());
 
 window.onbeforeunload = () => {
-  localStorage.setItem('lang',store.getState().appPrefs.lang);
-}
+  localStorage.setItem("lang", store.getState().appPrefs.lang);
+};
 
 class App extends Component {
-
   constructor() {
     super();
-
-	}
+  }
 
   render() {
-    return (
-      <PageContainer />
-    );
+    return <PageContainer />;
   }
 }
 
-
-render( <Provider store={store}><App/></Provider>, document.getElementById('app') );
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
