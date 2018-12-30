cab91264d1805299ec1167341b2ae16e22dbd626
biancaseufert <biancaseufert@toasthub>
2018-12-30 10:37:12 -0500

    update babel

--- cborgreact/webpack.common.js
+++ cborgreact/webpack.common.js
@@ -16,14 +16,13 @@ module.exports = {
         test : /\.jsx?/,
         include: APP_DIR,
         exclude: /node_modules/,
-        loader: 'eslint-loader'
+        use: { loader: 'eslint-loader' }
       },
       {
         test : /\.jsx?/,
         include : APP_DIR,
         exclude : /node_modules/,
-        loader : 'babel-loader',
-        query: { presets: ['es2015', 'react'] }
+        use: { loader : 'babel-loader' }
       },
       {
         test: /\.css$/,
