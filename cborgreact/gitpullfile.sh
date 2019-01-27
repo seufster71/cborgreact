219ca515b6164d4f58d1d97d0a89bffdad78d7c6
Bianca Seufert <biancaseufert@Biancas-MacBook-Pro-2.local>
2019-01-27 10:20:45 -0500

    Dashboard Upgrade

--- cborgreact/gitpullfile.sh
+++ cborgreact/gitpullfile.sh
@@ -1,106 +1,106 @@
 ECHO admin
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/admin
-git pull
+git pull origin master
 
 ECHO adminView
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/adminView
-git pull
+git pull origin master
 
 ECHO core
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/core
-git pull
+git pull origin master
 
 ECHO coreView
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/coreView
-git pull
+git pull origin master
 
 ECHO member acquaintances
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/acquaintances
-git pull
+git pull origin master
 
 ECHO member dashboard
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/dashboard
-git pull
+git pull origin master
 
 ECHO member events
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/events
-git pull
+git pull origin master
 
 ECHO member groups
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/groups
-git pull
+git pull origin master
 
 ECHO member logout
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/logout
-git pull
+git pull origin master
 
 ECHO member notes
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/notes
-git pull
+git pull origin master
 
 ECHO member profile
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/profile
-git pull
+git pull origin master
 
 ECHO member session
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/session
-git pull
+git pull origin master
 
 ECHO member shopping
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/shopping
-git pull
+git pull origin master
 
 ECHO member submenu
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/submenu
-git pull
+git pull origin master
 
 
 ECHO memberView acquaintances
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/acquaintances
-git pull
+git pull origin master
 
 ECHO memberView dashboard
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/dashboard
-git pull
+git pull origin master
 
 ECHO memberView events
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/events
-git pull
+git pull origin master
 
 ECHO memberView groups
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/groups
-git pull
+git pull origin master
 
 ECHO memberView logout
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/logout
-git pull
+git pull origin master
 
 ECHO memberView notes
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/notes
-git pull
+git pull origin master
 
 ECHO memberView profile
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/profile
-git pull
+git pull origin master
 
 ECHO memberView shopping
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/shopping
-git pull
+git pull origin master
 
 ECHO memberView submenu
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/submenu
-git pull
+git pull origin master
 
 
 ECHO system
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/system
-git pull
+git pull origin master
 
 ECHO systemView
 cd ~/gitsts/cborgreact/cborgreact/src/main/js/systemView
-git pull
+git pull origin master
 
 
 ECHO main
 cd ~/gitsts/cborgreact/cborgreact
-git pull
+git pull origin master
