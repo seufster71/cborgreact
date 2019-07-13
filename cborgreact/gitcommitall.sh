12be7e3e322b7f9fd4fb2057dc37dae417dcb026
Edward Seufert <edwardseufert@Edwards-MBP.home>
2019-07-12 20:20:09 -0400

    initial setup

--- cborgreact/gitcommitall.sh
+++ cborgreact/gitcommitall.sh
@@ -19,109 +19,109 @@ checkAndCommit() {
 
 commitAll() {
 	ECHO "#### admin"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/admin
+	cd ~/git/cborgreact/cborgreact/src/main/js/admin
 	checkAndCommit "${1}"
 
 	ECHO "#### adminView"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/adminView
+	cd ~/git/cborgreact/cborgreact/src/main/js/adminView
 	checkAndCommit "${1}"
 	
 	ECHO "#### core"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/core
+	cd ~/git/cborgreact/cborgreact/src/main/js/core
 	checkAndCommit "${1}"
 	
 	ECHO "#### coreView"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/coreView
+	cd ~/git/cborgreact/cborgreact/src/main/js/coreView
 	checkAndCommit "${1}"
 	
 	ECHO "#### member acquaintances"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/acquaintances
+	cd ~/git/cborgreact/cborgreact/src/main/js/member/acquaintances
 	checkAndCommit "${1}"
 	
 	ECHO "#### member dashboard"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/dashboard
+	cd ~/git/cborgreact/cborgreact/src/main/js/member/dashboard
 	checkAndCommit "${1}"
 	
 	ECHO "#### member events"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/events
+	cd ~/git/cborgreact/cborgreact/src/main/js/member/events
 	checkAndCommit "${1}"
 	
 	ECHO "#### member groups"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/groups
+	cd ~/git/cborgreact/cborgreact/src/main/js/member/groups
 	checkAndCommit "${1}"
 	
 	ECHO "#### member logout"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/logout
+	cd ~/git/cborgreact/cborgreact/src/main/js/member/logout
 	checkAndCommit "${1}"
 	
 	ECHO "#### member notes"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/notes
+	cd ~/git/cborgreact/cborgreact/src/main/js/member/notes
 	checkAndCommit "${1}"
 	
 	ECHO "#### member profile"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/profile
+	cd ~/git/cborgreact/cborgreact/src/main/js/member/profile
 	checkAndCommit "${1}"
 	
 	ECHO "#### member session"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/session
+	cd ~/git/cborgreact/cborgreact/src/main/js/member/session
 	checkAndCommit "${1}"
 	
 	ECHO "#### member shopping"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/shopping
+	cd ~/git/cborgreact/cborgreact/src/main/js/member/shopping
 	checkAndCommit "${1}"
 	
 	ECHO "#### member submenu"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/submenu
+	cd ~/git/cborgreact/cborgreact/src/main/js/member/submenu
 	checkAndCommit "${1}"
 	
 	ECHO "#### memberView acquaintances"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/acquaintances
+	cd ~/git/cborgreact/cborgreact/src/main/js/memberView/acquaintances
 	checkAndCommit "${1}"
 	
 	ECHO "#### memberView dashboard"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/dashboard
+	cd ~/git/cborgreact/cborgreact/src/main/js/memberView/dashboard
 	checkAndCommit "${1}"
 	
 	ECHO "#### memberView events"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/events
+	cd ~/git/cborgreact/cborgreact/src/main/js/memberView/events
 	checkAndCommit "${1}"
 	
 	ECHO "#### memberView groups"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/groups
+	cd ~/git/cborgreact/cborgreact/src/main/js/memberView/groups
 	checkAndCommit "${1}"
 	
 	ECHO "#### memberView logout"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/logout
+	cd ~/git/cborgreact/cborgreact/src/main/js/memberView/logout
 	checkAndCommit "${1}"
 	
 	ECHO "#### memberView notes"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/notes
+	cd ~/git/cborgreact/cborgreact/src/main/js/memberView/notes
 	checkAndCommit "${1}"
 	
 	ECHO "#### memberView profile"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/profile
+	cd ~/git/cborgreact/cborgreact/src/main/js/memberView/profile
 	checkAndCommit "${1}"
 	
 	ECHO "#### memberView shopping"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/shopping
+	cd ~/git/cborgreact/cborgreact/src/main/js/memberView/shopping
 	checkAndCommit "${1}"
 	
 	ECHO "#### memberView submenu"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/submenu
+	cd ~/git/cborgreact/cborgreact/src/main/js/memberView/submenu
 	checkAndCommit "${1}"
 	
 	
 	ECHO "#### system"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/system
+	cd ~/git/cborgreact/cborgreact/src/main/js/system
 	checkAndCommit "${1}"
 	
 	ECHO "#### systemView"
-	cd ~/gitsts/cborgreact/cborgreact/src/main/js/systemView
+	cd ~/git/cborgreact/cborgreact/src/main/js/systemView
 	checkAndCommit "${1}"
 	
 	
 	ECHO "#### main"
-	cd ~/gitsts/cborgreact/cborgreact
+	cd ~/git/cborgreact/cborgreact
 	checkAndCommit "${1}"
 	
 	ECHO "#### Done committing"
--- cborgreact/gitpullfile.sh
+++ cborgreact/gitpullfile.sh
@@ -1,106 +1,106 @@
 ECHO admin
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/admin
+cd ~/git/cborgreact/cborgreact/src/main/js/admin
 git pull origin master
 
 ECHO adminView
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/adminView
+cd ~/git/cborgreact/cborgreact/src/main/js/adminView
 git pull origin master
 
 ECHO core
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/core
+cd ~/git/cborgreact/cborgreact/src/main/js/core
 git pull origin master
 
 ECHO coreView
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/coreView
+cd ~/git/cborgreact/cborgreact/src/main/js/coreView
 git pull origin master
 
 ECHO member acquaintances
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/acquaintances
+cd ~/git/cborgreact/cborgreact/src/main/js/member/acquaintances
 git pull origin master
 
 ECHO member dashboard
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/dashboard
+cd ~/git/cborgreact/cborgreact/src/main/js/member/dashboard
 git pull origin master
 
 ECHO member events
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/events
+cd ~/git/cborgreact/cborgreact/src/main/js/member/events
 git pull origin master
 
 ECHO member groups
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/groups
+cd ~/git/cborgreact/cborgreact/src/main/js/member/groups
 git pull origin master
 
 ECHO member logout
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/logout
+cd ~/git/cborgreact/cborgreact/src/main/js/member/logout
 git pull origin master
 
 ECHO member notes
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/notes
+cd ~/git/cborgreact/cborgreact/src/main/js/member/notes
 git pull origin master
 
 ECHO member profile
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/profile
+cd ~/git/cborgreact/cborgreact/src/main/js/member/profile
 git pull origin master
 
 ECHO member session
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/session
+cd ~/git/cborgreact/cborgreact/src/main/js/member/session
 git pull origin master
 
 ECHO member shopping
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/shopping
+cd ~/git/cborgreact/cborgreact/src/main/js/member/shopping
 git pull origin master
 
 ECHO member submenu
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/member/submenu
+cd ~/git/cborgreact/cborgreact/src/main/js/member/submenu
 git pull origin master
 
 
 ECHO memberView acquaintances
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/acquaintances
+cd ~/git/cborgreact/cborgreact/src/main/js/memberView/acquaintances
 git pull origin master
 
 ECHO memberView dashboard
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/dashboard
+cd ~/git/cborgreact/cborgreact/src/main/js/memberView/dashboard
 git pull origin master
 
 ECHO memberView events
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/events
+cd ~/git/cborgreact/cborgreact/src/main/js/memberView/events
 git pull origin master
 
 ECHO memberView groups
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/groups
+cd ~/git/cborgreact/cborgreact/src/main/js/memberView/groups
 git pull origin master
 
 ECHO memberView logout
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/logout
+cd ~/git/cborgreact/cborgreact/src/main/js/memberView/logout
 git pull origin master
 
 ECHO memberView notes
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/notes
+cd ~/git/cborgreact/cborgreact/src/main/js/memberView/notes
 git pull origin master
 
 ECHO memberView profile
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/profile
+cd ~/git/cborgreact/cborgreact/src/main/js/memberView/profile
 git pull origin master
 
 ECHO memberView shopping
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/shopping
+cd ~/git/cborgreact/cborgreact/src/main/js/memberView/shopping
 git pull origin master
 
 ECHO memberView submenu
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/memberView/submenu
+cd ~/git/cborgreact/cborgreact/src/main/js/memberView/submenu
 git pull origin master
 
 
 ECHO system
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/system
+cd ~/git/cborgreact/cborgreact/src/main/js/system
 git pull origin master
 
 ECHO systemView
-cd ~/gitsts/cborgreact/cborgreact/src/main/js/systemView
+cd ~/git/cborgreact/cborgreact/src/main/js/systemView
 git pull origin master
 
 
 ECHO main
-cd ~/gitsts/cborgreact/cborgreact
+cd ~/git/cborgreact/cborgreact
 git pull origin master
