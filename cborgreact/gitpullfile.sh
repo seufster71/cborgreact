bbe1ff90e2b5d730fbffd0dce2664d83aaef61a3
seufster71 <seufster@gmail.com>
2022-10-26 10:50:14 -0400

    update version

--- cborgreact/gitpullfile.sh
+++ cborgreact/gitpullfile.sh
@@ -28,38 +28,6 @@ cd ~/$GIT_DIR/cborgreact/cborgreact/src/main/js/memberView/social
 git pull origin $BRANCH
 
 
-ECHO member dashboard
-cd ~/$GIT_DIR/cborgreact/cborgreact/src/main/js/member/dashboard
-git pull origin $BRANCH
-
-ECHO memberView dashboard
-cd ~/$GIT_DIR/cborgreact/cborgreact/src/main/js/memberView/dashboard
-git pull origin $BRANCH
-
-
-ECHO member logout
-cd ~/$GIT_DIR/cborgreact/cborgreact/src/main/js/member/logout
-git pull origin $BRANCH
-
-ECHO memberView logout
-cd ~/$GIT_DIR/cborgreact/cborgreact/src/main/js/memberView/logout
-git pull origin $BRANCH
-
-
-ECHO member profile
-cd ~/$GIT_DIR/cborgreact/cborgreact/src/main/js/member/profile
-git pull origin $BRANCH
-
-ECHO memberView profile
-cd ~/$GIT_DIR/cborgreact/cborgreact/src/main/js/memberView/profile
-git pull origin $BRANCH
-
-
-ECHO member session
-cd ~/$GIT_DIR/cborgreact/cborgreact/src/main/js/member/session
-git pull origin $BRANCH
-
-
 ECHO member pm
 cd ~/$GIT_DIR/cborgreact/cborgreact/src/main/js/member/pm
 git pull origin $BRANCH
